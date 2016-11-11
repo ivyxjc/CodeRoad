f9d5ae0387fadba8aa2167e97bb99a507457735b
ivyxjc <ivyxjc1994@hotmail.com>
2016-11-11 17:00:57 +0800

    factory pattern

---
+++ src/factory/factory.java
@@ -0,0 +1,46 @@
+package factory;
+
+/**
+ * Created by 2438981 on 2016/11/11.
+ */
+public class factory {
+}
+
+
+abstract class Product{
+    public void method(){
+
+    }
+    public abstract void method2();
+}
+
+
+class ConcreteProduct1 extends Product{
+    public void method2(){
+
+    }
+}
+
+class ConcreteProduct2 extends Product{
+    public void method2(){
+
+    }
+}
+
+
+
+abstract class Creator{
+    public abstract <T extends Product> T createProduct(Class<T> c);
+}
+
+class ConcreteCreator extends Creator{
+    public <T extends Product> T createProduct(Class<T> c){
+        Product product=null;
+        try{
+            product=(Product)Class.forName(c.getName()).newInstance();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return (T)product;
+    }
+}
