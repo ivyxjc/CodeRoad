41a117d0b64de36e3e53dc4765afab819d8ddf49
ivyxjc <ivyxjc1994@hotmail.com>
2016-11-25 09:19:18 +0800

    merge

--- src/stone/Lexer.java
+++ src/stone/Lexer.java
@@ -1,12 +1,9 @@
 
 package stone;
 
-<<<<<<< HEAD
-import stone.Exception.ParseException;
-=======
+
 import stone.exception.ParseException;
 
->>>>>>> bc68bc747d65bf3fe0b8e824a5cd8704a756b15f
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.Reader;
@@ -19,15 +16,12 @@ public class Lexer {
     public static String regexPat
             = "\\s*((//.*)|([0-9]+)|(\"(\\\\\"|\\\\\\\\|\\\\n|[^\"])*\")"
             + "|[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\\|\\||\\p{Punct})?";
-<<<<<<< HEAD
-    private Pattern pattern = Pattern.compile(regexPat);
-    private ArrayList<Token> queue = new ArrayList<Token>();
-=======
+
 
     public Pattern pattern=Pattern.compile(regexPat);
 
     public ArrayList<Token> queue=new ArrayList<>();
->>>>>>> bc68bc747d65bf3fe0b8e824a5cd8704a756b15f
+
     private boolean hasMore;
     private LineNumberReader reader;
 
@@ -69,84 +63,6 @@ public class Lexer {
         int lineNo = reader.getLineNumber();
         Matcher matcher = pattern.matcher(line);
         matcher.useTransparentBounds(true).useAnchoringBounds(false);
-<<<<<<< HEAD
-        int pos = 0;
-        int endPos = line.length();
-        while (pos < endPos) {
-            matcher.region(pos, endPos);
-            if (matcher.lookingAt()) {
-                addToken(lineNo, matcher);
-                pos = matcher.end();
-            }
-            else
-                throw new ParseException("bad token at line " + lineNo);
-        }
-        queue.add(new IdToken(lineNo, Token.EOL));
-    }
-    protected void addToken(int lineNo, Matcher matcher) {
-        String m = matcher.group(1);
-        if (m != null) // if not a space
-            if (matcher.group(2) == null) { // if not a comment
-                Token token;
-                if (matcher.group(3) != null)
-                    token = new NumToken(lineNo, Integer.parseInt(m));
-                else if (matcher.group(4) != null)
-                    token = new StrToken(lineNo, toStringLiteral(m));
-                else
-                    token = new IdToken(lineNo, m);
-                queue.add(token);
-            }
-    }
-    protected String toStringLiteral(String s) {
-        StringBuilder sb = new StringBuilder();
-        int len = s.length() - 1;
-        for (int i = 1; i < len; i++) {
-            char c = s.charAt(i);
-            if (c == '\\' && i + 1 < len) {
-                int c2 = s.charAt(i + 1);
-                if (c2 == '"' || c2 == '\\')
-                    c = s.charAt(++i);
-                else if (c2 == 'n') {
-                    ++i;
-                    c = '\n';
-                }
-            }
-            sb.append(c);
-        }
-        return sb.toString();
-    }
-
-    protected static class NumToken extends Token {
-        private int value;
-
-        protected NumToken(int line, int v) {
-            super(line);
-            value = v;
-        }
-        public boolean isNumber() { return true; }
-        public String getText() { return Integer.toString(value); }
-        public int getNumber() { return value; }
-    }
-
-    protected static class IdToken extends Token {
-        private String text;
-        protected IdToken(int line, String id) {
-            super(line);
-            text = id;
-        }
-        public boolean isIdentifier() { return true; }
-        public String getText() { return text; }
-    }
-
-    protected static class StrToken extends Token {
-        private String literal;
-        StrToken(int line, String str) {
-            super(line);
-            literal = str;
-        }
-        public boolean isString() { return true; }
-        public String getText() { return literal; }
-=======
 
         int pos = 0;
         int endPos = line.length();
@@ -274,6 +190,5 @@ public class Lexer {
         public boolean isString() {
             return true;
         }
->>>>>>> bc68bc747d65bf3fe0b8e824a5cd8704a756b15f
     }
 }
--- src/stone/runner/LexerRunner.java
+++ src/stone/runner/LexerRunner.java
@@ -5,9 +5,6 @@ import stone.Lexer;
 import stone.Token;
 import stone.exception.ParseException;
 
-/**
- * Created by jc on 11/21/2016.
- */
 public class LexerRunner {
     public static void main(String[] args) throws ParseException{
         Lexer l=new Lexer(new CodeDialog());
