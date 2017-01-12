29028f9ed0233f7c52facf1a2aca53f1e14dd664
ivyxjc <ivyxjc1994@hotmail.com>
2017-01-12 16:44:34 +0800

    access

---
+++ src/main/java/xyz/ivyxjc/access/Base.java
@@ -0,0 +1,23 @@
+package xyz.ivyxjc.access;
+
+/**
+ * Created by ivxyjc on 2017/1/12.
+ */
+public class Base {
+    private Base(){
+
+    }
+
+    public Base(String s){
+
+    }
+//    public
+}
+
+
+class Son extends Base{
+    public Son(String s) {
+        //super(); 编译错误,
+        super(s);
+    }
+}
