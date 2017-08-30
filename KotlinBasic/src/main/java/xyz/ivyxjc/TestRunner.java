2e2c2e142f3ccf3ee935e96d4a261c15844e4ae1
ivyxjc <ivyxjc1994@hotmail.com>
2017-08-30 23:38:09 +0800

    commit before merge github java

---
+++ KotlinBasic/src/main/java/xyz/ivyxjc/TestRunner.java
@@ -0,0 +1,40 @@
+package xyz.ivyxjc;
+
+public class TestRunner {
+    static int run(Expr e) {
+        if (e instanceof Num) {
+            return ((Num) e).value;
+        }
+        if (e instanceof Sum) {
+            return run(((Sum) e).left) + run(((Sum) e).right);
+        }
+        throw new IllegalArgumentException("Unknown Expression");
+    }
+
+    public static void main(String[] args) {
+        int s = run(new Sum(new Sum(new Num(1), new Num(2)), new Num(4)));
+        System.out.println(s);
+    }
+}
+
+interface Expr {
+
+}
+
+class Sum implements Expr {
+    Expr left;
+    Expr right;
+
+    public Sum(Expr left, Expr right) {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Num implements Expr {
+    int value;
+
+    public Num(int value) {
+        this.value = value;
+    }
+}
---
+++ KotlinBasic/src/main/kotlin/basic.kt
@@ -0,0 +1,42 @@
+data class Person(val name: String, val age: Int? = null)
+//
+//fun main(args: Array<String>) {
+//    val persons= listOf(Person("Alice"),Person("Bob",age = 20))
+//    val oldest=persons.maxBy { it.age?:0 }
+//    println("The oldest is: $oldest")
+//}
+
+class Rectangle(val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
+
+    var hint: Boolean
+        get() {
+            return height == width
+        }
+        set(value) {
+            hint = value
+        }
+}
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        val n = e
+        return n.value
+    }
+    if (e is Sum) {
+        return eval(e.left) + eval(e.right)
+    }
+    throw  IllegalArgumentException("Unknown expression")
+}
+
+fun main(args: Array<String>) {
+    eval(Sum(Sum(Num(1), Num(2)), Num(4)))
+}
+
