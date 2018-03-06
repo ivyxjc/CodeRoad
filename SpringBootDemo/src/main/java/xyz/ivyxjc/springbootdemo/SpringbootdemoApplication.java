30743792a1e5599b69c332d04ba8ab4a105da7f1
ivyxjc <ivyxjc1994@hotmail.com>
2018-03-07 00:03:59 +0800

    first application of SpringBoot

---
+++ SpringBootDemo/src/main/java/xyz/ivyxjc/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,28 @@
+package xyz.ivyxjc.springbootdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return t -> {
+            String[] beanNames = context.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String name : beanNames) {
+                System.out.println(name);
+            }
+        };
+    }
+}
---
+++ SpringBootDemo/src/main/java/xyz/ivyxjc/springbootdemo/web/HelloController.java
@@ -0,0 +1,15 @@
+package xyz.ivyxjc.springbootdemo.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
+
+}
